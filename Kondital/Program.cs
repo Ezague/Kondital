5652545c8c6f5255ffa23030342fa596ca111993
Ezague <kristian.petersen@lego.com>
2022-12-05 15:58:34 +0100

    error handling on data

--- Kondital/Program.cs
+++ Kondital/Program.cs
@@ -6,10 +6,20 @@ class Program
 {
     public static void Main()
     {
+        int intMaxPuls = -1;
+        int intHvilePuls = -1;
         Console.Write("Indtast din max puls i heltal slag i minuttet: ");
-        int maxPuls = Convert.ToInt32(Console.ReadLine());
+        string? MaxPuls = Console.ReadLine();
+        if (int.TryParse(MaxPuls, out int a))
+        {
+            intMaxPuls = a;
+        }
         Console.Write("Indtast din hvilepuls i heltal slag i minuttet: ");
-        int hvilePuls = Convert.ToInt32(Console.ReadLine());
+        string? HvilePuls = Console.ReadLine();
+        if (int.TryParse(HvilePuls, out int b))
+        {
+            intHvilePuls = b;
+        }
     }
 }
 
