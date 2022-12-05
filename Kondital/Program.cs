32625d7bfc1b85a02c6c0d2aa1dbe85809d1177f
Ezague <kristian.petersen@lego.com>
2022-12-05 15:36:30 +0100

    Indlæsning af bruger data

--- Kondital/Program.cs
+++ Kondital/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Kondital;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Indtast din max puls i heltal slag i minuttet: ");
+        int maxPuls = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Indtast din hvilepuls i heltal slag i minuttet: ");
+        int hvilePuls = Convert.ToInt32(Console.ReadLine());
+    }
+}
 
